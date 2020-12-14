e1781de6820d396cd7cdaa705431985a0476fa63
jiaruifang <jiaruifang@tencent.com>
2020-12-14 19:38:39 +0800

    add a cuda example

--- dlsym_hook.h
+++ dlsym_hook.h
@@ -7,12 +7,12 @@
 extern "C" {
 
 int cuMemFree_v2(uintptr_t ptr) {
-    printf("cuMemFree_v2");
+    printf("cuMemFree_v2\n");
     return 0;
 }
 
 int cuMemAlloc_v2(uintptr_t *devPtr, size_t size) {
-    printf("cuMemAlloc_v2");
+    printf("cuMemAlloc_v2\n");
     return 0;
 }
 
@@ -21,6 +21,7 @@ void *dlsym(void * handle, const char * symbol) __DYLDDL_DRIVERKIT_UNAVAILABLE {
 #else
 void *dlsym(void *handle, const char *symbol) noexcept {
 #endif
+    printf("dlsym loading %s\n", symbol);
     if (strncmp(symbol, "cu", 2) != 0) {
         return wxgpumemmgr::real_dlsym(handle, symbol);
     } else if (strcmp(symbol, "cuMemAlloc_v2") == 0) {
