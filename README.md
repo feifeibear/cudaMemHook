373cf59cb16af57e8d474a950c40ae453313e79f
jiaruifang <jiaruifang@tencent.com>
2020-12-15 10:46:06 +0800

    polish code

--- README.md
+++ README.md
@@ -4,5 +4,6 @@
 `mkdir -p build && cd build && cmake ..`
 
 run an example
-
-`cd example && make run`
+`cd  example`
+`make run`
+`env LD_PRELOAD=../build/libcuda_hooker.so python test.py`
--- dlsymhooker.cpp
+++ dlsymhooker.cpp
@@ -11,8 +11,22 @@
 // permissions and limitations under the License.
 // See the AUTHORS file for names of contributors.
 
-//
-// Created by Jiarui Fang on 2020/12/14.
-//
-
 #include "dlsymhooker.h"
+#include "cudahooker.hpp"
+#include "realdlsym.h"
+
+extern "C" {
+#ifdef __APPLE__
+void *dlsym(void * handle, const char * symbol) __DYLDDL_DRIVERKIT_UNAVAILABLE {
+#else
+void *dlsym(void *handle, const char *symbol) noexcept {
+#endif
+    auto& hooker = wxgpumemmgr::CudaHook::instance();
+    if (hooker.IsValid(symbol)) {
+      printf("hooking %s\n", symbol);
+        return hooker.GetFunction(symbol);
+    }
+    printf("dlsym loading %s\n", symbol);
+    return wxgpumemmgr::real_dlsym(handle, symbol);
+}
+} // extern "C"
