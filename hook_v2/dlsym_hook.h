fd9e1dd1e66fc358bb7a5bb4f0429510abe2a15d
jiayifeng <jiayifeng@tencent.com>
2021-01-07 20:14:29 +0800

    refine folder struct

--- hook_v2/dlsym_hook.h
+++ hook_v2/dlsym_hook.h
@@ -11,21 +11,12 @@
 // permissions and limitations under the License.
 // See the AUTHORS file for names of contributors.
 
-#include "dlsym_hooker.h"
-#include "cuda_alloc_client.h"
-#include "real_dlsym.h"
+#pragma once
 
-extern "C" {
+#include <dlfcn.h>
+
 #ifdef __APPLE__
-void *dlsym(void *handle, const char *symbol) __DYLDDL_DRIVERKIT_UNAVAILABLE {
+void *dlsym(void *handle, const char *symbol) __DYLDDL_DRIVERKIT_UNAVAILABLE;
 #else
-void *dlsym(void *handle, const char *symbol) noexcept {
+void *dlsym(void *handle, const char *symbol) noexcept;
 #endif
-  static auto *real_dlsym = turbo_hooker::GetRealDlsym();
-  void *func = turbo_hook::service::Dlsym(handle, symbol);
-  if (func != nullptr) {
-    return func;
-  }
-  return real_dlsym(handle, symbol);
-}
-}
--- hooker_v2/real_dlsym.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace turbo_hooker {
-
-using DlsymFn = void *(void *, const char *);
-
-DlsymFn *GetRealDlsym() noexcept;
-
-}
